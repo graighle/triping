4672ec2f02faf888c8e4cfdc550d5d6a745dc172
Shogo Furusawa <graighle@gmail.com>
2019-05-19 06:49:32 +0900

    ユーザデータのフォーマット例外追加。

---
+++ TripingCore/Exceptions/UserDataFormatException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Graighle.Triping.Exceptions
+{
+    /// <summary>
+    /// ユーザデータファイルのフォーマットエラー。
+    /// </summary>
+    [Serializable()]
+    public class UserDataFormatException : Exception
+    {
+        public UserDataFormatException()
+            : base()
+        {
+        }
+
+        public UserDataFormatException(string message)
+            : base(message)
+        {
+        }
+
+        public UserDataFormatException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        /// <summary>
+        /// 逆シリアライズ用。
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
+        protected UserDataFormatException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
