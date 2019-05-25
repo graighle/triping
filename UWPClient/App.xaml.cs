5dda054aa856003ae1fdd51d02f1d1ada9621bf6
Shogo Furusawa <graighle@gmail.com>
2019-05-25 18:26:46 +0900

    トップナビゲーションのタブ名をリソース化。

--- UWPClient/App.xaml.cs
+++ UWPClient/App.xaml.cs
@@ -1,18 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 namespace Graighle.Triping.UWPClient
@@ -53,6 +44,10 @@ namespace Graighle.Triping.UWPClient
                 if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
                 {
                     //TODO: 以前中断したアプリケーションから状態を読み込みます
+                }
+                else
+                {
+                    var quota = ApplicationData.Current.RoamingStorageQuota;
                 }
 
                 // フレームを現在のウィンドウに配置します
