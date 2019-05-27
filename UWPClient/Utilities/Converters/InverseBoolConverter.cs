30084959e252b737b77e091de219f9fb3cee98f0
Shogo Furusawa <graighle@gmail.com>
2019-05-28 00:40:08 +0900

    bool反転コンバータ追加。

---
+++ UWPClient/Utilities/Converters/InverseBoolConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace Graighle.Triping.UWPClient.Utilities.Converters
+{
+    /// <summary>
+    /// Booleanを反転する変換。
+    /// </summary>
+    public class InverseBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return !(bool)value;
+        }
+    }
+}
