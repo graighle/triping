ac6bd4d897246431c5d80c20353554a32281c563
Shogo Furusawa <graighle@gmail.com>
2019-06-05 21:04:58 +0900

    外部データ操作エラークラスを追加。

---
+++ TripingCore/Exceptions/ExternalDataOperationException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Graighle.Triping.Exceptions
+{
+    /// <summary>
+    /// システム外データの操作エラー。
+    /// </summary>
+    [Serializable()]
+    public class ExternalDataOperationException : Exception
+    {
+        public ExternalDataOperationException()
+            : base()
+        {
+        }
+
+        public ExternalDataOperationException(string message)
+            : base(message)
+        {
+        }
+
+        public ExternalDataOperationException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        /// <summary>
+        /// 逆シリアライズ用。
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
+        protected ExternalDataOperationException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
