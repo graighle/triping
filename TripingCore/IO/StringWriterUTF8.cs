a96acc981d307ea3e287eebd1ea1763dbc60e2e2
Shogo Furusawa <graighle@gmail.com>
2019-06-05 21:05:40 +0900

    XML用にUTF-8用のStringWriterクラスを追加。

---
+++ TripingCore/IO/StringWriterUTF8.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Graighle.Triping.IO
+{
+    /// <summary>
+    /// UTF8エンコーディングを指定したStringWriter。
+    /// </summary>
+    class StringWriterUTF8 : StringWriter
+    {
+        public override Encoding Encoding => Encoding.UTF8;
+    }
+}
